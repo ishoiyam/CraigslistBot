073a17d32543e3b2796f35946b7f39fe1115d5b5
IshoWhoCodes <coderisho@localhost.localdomain>
2020-09-14 16:29:23 +0100

    final test

--- CraigBot.py
+++ CraigBot.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.weddriver.common.by import By
+from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
 import urllib.request
